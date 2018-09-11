cf0bc214dc4e5aa966c7bc0b854cbefd7782dce3
Unknown <benjamin.strabach@live.fr>
2018-09-11 16:27:23 +0200

    Début page d'accueil utilisateur

---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="page_home.css" rel="stylesheet" type="text/css"/>
+    <title>Home</title>
+</head>
+
+<body>
+<!-- <div id="fond_ecran" class="fond_ecran"></div> -->
+    <div class="fg">
+        <div class="home_header">
+            <div class="user_info">
+                <img class="profile_pict_small" src="medias/user.jpg">
+                <p class="profile_name">Benjamin STRABACH</p>
+            </div>
+            <div class="searchbar">
+                <input class="search_bar" type="text" placeholder="Rechercher">
+                <input class="search-btn" type="submit" value="Rechercher">
+            </div>
+            <div class="disconnect">
+                <input class="disconnect-btn" type="button" value="Déconnexion">
+            </div>
+        </div>
+        <div class="search_list">
+            <div class="list">
+                
+            </div>
+            <div class="list">
+                <strong class="place_name">Nom restaurant</strong>
+                <p class="place_address">Adresse</p>
+                <div class="category">
+                    <img class="category_pict" src="medias/logo_category.png">
+                    <p class="category_text">Vegan</p>
+                </div>
+                <div class="token_score">
+                    
+                </div>
+            </div>
+            <div class="list">
+                
+            </div>
+            <div class="list">
+                
+            </div>
+            <div class="list">
+                
+            </div>
+        </div>
+    </div>
+
+</body>
+
